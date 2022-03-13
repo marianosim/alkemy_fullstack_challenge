691ec67e1badf5f3323ba6840ca16e91028b436a
marianosim <mariano.simantob@gmail.com>
2022-03-13 17:06:25 -0300

    changing folder structure

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
 const apiRoutesActivities = require('./routes/API/activities');
 
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', indexRouter);
