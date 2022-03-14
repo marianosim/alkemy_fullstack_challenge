16ac565f1e618794d31fad0f5ae062c4c8e4ac90
marianosim <mariano.simantob@gmail.com>
2022-03-14 14:14:37 -0300

    Added new APIs routes

--- Backend/src/routes/API/activities.js
+++ Backend/src/routes/API/activities.js
@@ -6,5 +6,14 @@ const activitiesAPIController = require('../../controllers/API/activitiesAPICont
 //Listado de movimientos
 router.get('/api/activities', activitiesAPIController.list);
 
+//Crear movimiento nuevo
+router.get('/api/activities/create', activitiesAPIController.create);
+
+//Modificar movimiento
+router.get('/api/activities/edit/:id', activitiesAPIController.update);
+
+//Borrar registro
+router.get('/api/activities/delete/:id', activitiesAPIController.destroy);
+
 
 module.exports = router;
